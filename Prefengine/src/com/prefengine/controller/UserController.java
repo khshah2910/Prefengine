9d8d47bc6e2f4957df072b682408145e213967cc
Kush Shah <kush.shah001@umb.edu>
2017-03-10 14:49:16 -0500

    x'Initial check

---
+++ Prefengine/src/com/prefengine/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.prefengine.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.prefengine.model.User;
+import com.prefengine.dao.RegestrationDAO;
+
+public class UserController extends HttpServlet {
+    private static String INSERT = "/web/Register.jsp";
+    private RegestrationDAO dao;
+
+    
+ 
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	dao = new RegestrationDAO();
+    	User user = new User();
+        //user.setId(request.getParameter("id"));
+        user.setUserName(request.getParameter("userName"));
+        user.setEmail(request.getParameter("email"));
+        user.setPassword(request.getParameter("password"));
+        user.setSecQue(request.getParameter("s_que"));
+        user.setSecAns(request.getParameter("s_ans"));
+        dao.addUser(user);
+        RequestDispatcher rd= request.getRequestDispatcher("/web/login.jsp");
+        rd.forward(request, response);
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        RequestDispatcher rd= request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+    }
+
+}
